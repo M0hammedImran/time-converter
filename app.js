3ea6ffbf76a126eab22c6a392905e4418df945ec
Mohammed Imran <52968617+M0hammedImran@users.noreply.github.com>
2020-04-02 00:27:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const timeConverter = () => {
+  console.clear();
+  var hour = document.getElementById('hour').value;
+  var min = document.getElementById('min').value;
+  var sec = document.getElementById('sec').value;
+};
