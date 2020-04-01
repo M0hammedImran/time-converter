d016b09aea5c3985d92602a0247aef6f33d0d3ec
Mohammed Imran <52968617+M0hammedImran@users.noreply.github.com>
2020-04-02 02:53:10 +0530

    Math and Validation

--- public/app.js
+++ public/app.js
@@ -2,12 +2,23 @@ let errH = document.querySelector('.errMessageH');
 let errM = document.querySelector('.errMessageM');
 let errS = document.querySelector('.errMessageS');
 let btn = document.querySelector('.btn');
+let output = document.querySelector('#output');
 
-function isCorrect(hh, mm, ss) {
-  let oldHour = hh;
-  let oldMin = mm;
-  let oldSec = ss;
-  console.log(oldHour, oldMin, oldSec);
+function isCorrect(hh, mm, ss, aa) {
+  let hour = Number(hh),
+    min,
+    sec;
+
+  mm.length == 1 ? (min = 0 + mm) : (min = mm);
+  ss.length == 1 ? (sec = 0 + ss) : (sec = ss);
+
+  if (aa === 'AM' && hour == 12) {
+    let newHour = hour - 12;
+    output.value = `${newHour}:${min}:${sec}`;
+  } else if (aa === 'PM' && hour >= 1 && hour <= 11) {
+    let newHour = hour + 12;
+    output.value = `${newHour}:${min}:${sec}`;
+  } else output.value = `${hour}:${min}:${sec}`;
 }
 
 function timeConverter() {
@@ -19,13 +30,22 @@ function timeConverter() {
   var hour = document.getElementById('hour').value;
   var min = document.getElementById('min').value;
   var sec = document.getElementById('sec').value;
+  var period = document.getElementById('period').value;
 
   if (hour >= 1 && hour <= 12)
     if (min >= 0 && min <= 59)
-      if (sec >= 0 && sec <= 59) isCorrect(hour, min, sec);
+      if (sec >= 0 && sec <= 59) isCorrect(hour, min, sec, period);
       else errS.innerText = '0 ≤ Seconds ≤ 59';
     else errM.innerText = '0 ≤ Minutes ≤ 59';
   else errH.innerText = '1 ≤ Hour ≤ 12 ';
+
+  // hour >= 1 && hour <= 12
+  //   ? min >= 0 && min <= 59
+  //     ? sec >= 0 && sec <= 59
+  //       ? isCorrect(hour, min, sec, period)
+  //       : (errS.innerText = '0 ≤ Seconds ≤ 59')
+  //     : (errM.innerText = '0 ≤ Minutes ≤ 59')
+  //   : (rrH.innerText = '1 ≤ Hour ≤ 12 ');
 }
 
 btn.addEventListener('click', timeConverter);
